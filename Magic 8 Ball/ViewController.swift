3ed4950b09e996c9fde379a1814bdf2f21ddea2a
ishaansarna <ishaansarna23@gmail.com>
2021-08-31 16:07:23 +0530

    git commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -14,13 +14,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImage: UIImageView!
     
-    let ballArray = [#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball1")]
+    let ballArray = [#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball1")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let randomInt = Int.random(in: 1...5)
+    }
+    
+    var randomInt = Int.random(in: 0...4)
+    var prevRandomInt = -1
+    @IBAction func askClick(_ sender: Any) {
+        randomInt = Int.random(in: 0...4)
+        while randomInt == prevRandomInt {
+            randomInt = Int.random(in: 0...4)
+        }
         diceImage.image = ballArray[randomInt]
+        prevRandomInt = randomInt
     }
     
 }
