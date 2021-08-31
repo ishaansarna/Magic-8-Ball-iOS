6235fb057144022a936bffc3686fa1ff9656906f
ishaansarna <ishaansarna23@gmail.com>
2021-08-31 16:18:34 +0530

    final

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,11 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var askButton: UIButton!
-    
     @IBOutlet weak var diceImage: UIImageView!
-    
     let ballArray = [#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball5")]
     
     override func viewDidLoad() {
